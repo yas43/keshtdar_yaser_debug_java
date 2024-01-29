6ded0c1a3b14f6adee7f3d0589573da6ce561194
yaser <y.r.k.r.2011@gmail.com>
2024-01-29 17:36:31 +0100

    re modify

--- Project02Eclipse/src/com/hemebiotech/analytics/read_write/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/read_write/Main.java
@@ -14,8 +14,8 @@ public class Main {
         symptomReader = new ReadSymptomDataFromFile("keshtdar_yaser_debug_java/Project02Eclipse/symptoms.txt");
         symptomWriter = new WriteSymptomDataFromFile("result.out_test2");
         analyticsCounter = new AnalyticsCounter(symptomReader,symptomWriter);
-        List<String> list = analyticsCounter.getSymptoms();
-        Map<String,Long> counted_map = analyticsCounter.countSymptoms(list);
+        List<String> list = analyticsCounter.getSymptoms(); //read the file and save it in a list
+        Map<String,Long> counted_map = analyticsCounter.countSymptoms(list);//take file as a list and count occurrence for every item over a map
         Map<String,Long> sorted_map = analyticsCounter.sortSymptoms(counted_map);
         symptomWriter.WriteSymptoms(sorted_map);
 
